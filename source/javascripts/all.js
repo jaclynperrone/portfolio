8d77d192f24fc121bacf5fc23610694df887fcd6
joemclean <mclean.joe@gmail.com>
2014-12-13 19:12:22 -0500

    Add dupont iframe and more images.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,25 @@
 //= require_tree .
+
+$(function(){
+  
+  // var iframe = $('#dupont-iframe');
+
+  $('#breakpoint-select').change(function() {
+
+    var breakpointSelected = $('#breakpoint-select').val();
+
+    if ( breakpointSelected == 'desktop') {
+      $('#dupont-iframe').attr('width', '100%');
+    }
+    if ( breakpointSelected == 'tablet') {
+      $('#dupont-iframe').attr('width', '768');
+    }
+    if ( breakpointSelected == 'mobile') {
+      $('#dupont-iframe').attr('width', '480');
+    }
+  });
+
+});
+
+
+
