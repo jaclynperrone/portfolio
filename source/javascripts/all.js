5de0972a3c0f0c88805253483a1ef37e940a5410
joemclean <mclean.joe@gmail.com>
2014-12-28 22:30:44 -0500

    added custom breakpoint selection buttons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,19 +4,23 @@ $(function(){
   
   // var iframe = $('#dupont-iframe');
 
-  $('#breakpoint-select').change(function() {
-
-    var breakpointSelected = $('#breakpoint-select').val();
-
-    if ( breakpointSelected == 'desktop') {
-      $('#dupont-iframe').attr('width', '100%');
-    }
-    if ( breakpointSelected == 'tablet') {
-      $('#dupont-iframe').attr('width', '768');
-    }
-    if ( breakpointSelected == 'mobile') {
-      $('#dupont-iframe').attr('width', '480');
-    }
+  $('.desktop-button').click(function() {
+    $('.device-wrapper').css('width', '100%');
+    $('#dupont-iframe').attr('height', '600');
+    $('.device-wrapper').css("padding", "50px");
+    $('#trapezoid').css("opacity", "1");
+  });
+  $('.tablet-button').click(function() {
+    $('.device-wrapper').css('width', '868');
+    $('#dupont-iframe').attr('height', '600');
+    $('.device-wrapper').css("padding", "50px");
+    $('#trapezoid').css("opacity", "0.0");
+  });
+  $('.mobile-button').click(function() {
+    $('.device-wrapper').css('width', '520');
+    $('#dupont-iframe').attr('height', '760');
+    $('.device-wrapper').css("padding", " 50px 20px 70px 20px");
+    $('#trapezoid').css("opacity", "0.0");
   });
 
 });
