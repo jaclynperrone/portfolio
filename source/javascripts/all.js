637b2e8315389f423b92db42359814ca8adb1373
joemclean <mclean.joe@gmail.com>
2014-12-28 23:03:37 -0500

    active state on buttons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,14 +5,20 @@ $(function(){
   // var iframe = $('#dupont-iframe');
 
   $('.desktop-button').click(function() {
+    $('.breakpoint-button').removeClass('active');
+    $('.desktop-button').addClass('active');
     $('.device-wrapper').removeClass("tablet mobile").addClass("desktop");
     $('#trapezoid').css("opacity", "1");
   });
   $('.tablet-button').click(function() {
+    $('.breakpoint-button').removeClass('active');
+    $('.tablet-button').addClass('active');
     $('.device-wrapper').removeClass("desktop mobile").addClass("tablet");
     $('#trapezoid').css("opacity", "0.0");
   });
   $('.mobile-button').click(function() {
+    $('.breakpoint-button').removeClass('active');
+    $('.mobile-button').addClass('active');
     $('.device-wrapper').removeClass("desktop tablet").addClass("mobile");
     $('#trapezoid').css("opacity", "0.0");
   });
