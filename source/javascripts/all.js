107a30f4d2ea716a5d2118bdd0931a1a93e8b6c5
joemclean <mclean.joe@gmail.com>
2014-12-28 22:59:53 -0500

    refactoring css attributes to css file instead of js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,21 +5,15 @@ $(function(){
   // var iframe = $('#dupont-iframe');
 
   $('.desktop-button').click(function() {
-    $('.device-wrapper').css('width', '100%');
-    $('#dupont-iframe').attr('height', '600');
-    $('.device-wrapper').css("padding", "50px");
+    $('.device-wrapper').removeClass("tablet mobile").addClass("desktop");
     $('#trapezoid').css("opacity", "1");
   });
   $('.tablet-button').click(function() {
-    $('.device-wrapper').css('width', '868');
-    $('#dupont-iframe').attr('height', '600');
-    $('.device-wrapper').css("padding", "50px");
+    $('.device-wrapper').removeClass("desktop mobile").addClass("tablet");
     $('#trapezoid').css("opacity", "0.0");
   });
   $('.mobile-button').click(function() {
-    $('.device-wrapper').css('width', '520');
-    $('#dupont-iframe').attr('height', '760');
-    $('.device-wrapper').css("padding", " 50px 20px 70px 20px");
+    $('.device-wrapper').removeClass("desktop tablet").addClass("mobile");
     $('#trapezoid').css("opacity", "0.0");
   });
 
